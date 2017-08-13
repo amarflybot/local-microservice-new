2723a132975b6ab189b46cdfe030580243bbe9e5
Amarendra Kumar <amar.arawat@gmail.com>
2017-08-13 22:48:59 +0530

    Config-commit1

---
+++ stock-server/src/main/java/com/example/demo/StockServerApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import com.example.demo.model.Stock;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServerApplication.class, args);
+	}
+
+	@Bean
+    public CommandLineRunner commandLineRunner(final StockRepository stockRepository){
+	    return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                Arrays.asList("asdasd","asdasqwe","dturtuy").forEach(
+                        name-> {
+                            stockRepository.save(new Stock(name));
+                        });
+                stockRepository.findAll().forEach(stock -> {
+                    System.out.println(stock);
+                });
+            }
+        };
+    }
+}
